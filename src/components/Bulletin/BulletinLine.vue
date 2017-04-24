0e9a4a4999cfedf81d27ff56fc73a8b895431f26
yanxiaodi <929213769@qq.com>
2017-04-25 00:12:15 +0800

    :art: fixed 'BulletinLine.vue'; test success

--- src/components/Bulletin/BulletinLine.vue
+++ src/components/Bulletin/BulletinLine.vue
@@ -1,8 +1,9 @@
 // '物业公告' - 公告轴
 <template lang="pug">
 #BulletinLine
-    ul( 
-        v-infinite-scroll="loadMore"
+    ul(
+        v-if="this.$data.canRender"
+        v-infinite-scroll="countBulletin"
         infinite-scroll-disabled="loading"
         infinite-scroll-distance="51"
     )
@@ -10,9 +11,13 @@
             v-for="item in renderArr"
             v-bind:key="item.title"
         )
-            h2 {{ item.title }}
-            span {{ item.date }}
-            span {{ item.time }}
+            h2.auto--titleStyle {{ item.title }}
+            span.auto--textStyle {{ item.date }}
+            span.auto--textStyle {{ item.time }}
+
+    .BulletinLine--null
+        // img
+        h1 无消息
 </template>
 
 <script>
@@ -37,45 +42,76 @@ export default {
         }
     },
     methods: {
-        // 目的: 当 $props 传入, 计算公告数量: 如果 >10, 执行下拉加载效果; 如果 <= 10 无事件
-        countBulletinLength() {
-            let bulletinArr = this.$props.bulletinLineArr
-            if( bulletinArr.length > 10 ) {                            
-                for( let i = 0; i < 10; i++ ) {                        // 存入 10个公告
-                    this.$data.renderArr.push( bulletinArr[i])
-                }  
+        // 目的: 计算'公告'
+        countBulletin() {
+            let awaitNum = this.$data.bulletinNum - 10 * this.$data.loadNum             // 等待渲染'公告' 数
+            let multipleNum = parseInt( awaitNum / 10 )                                 // 等待渲染'公告' 是否是 10的倍数
+            multipleNum <= 0 ? this.loadMore( awaitNum ) : this.loadMore( 10 )
+        },
+        // 目的: 加载更多
+        loadMore( num ) {
+            if( this.$data.loadNum === 0 ) {                                            // 初次加载 不需等待
+                this.countRender( num )
+            } else {
+                if( this.$data.loadNum <= this.$data.maxMultiple ) {
+                    // console.log( '加载更多' )
+                    this.$data.loading = true
+                    setTimeout( () => {
+                        this.countRender( num )
+                        this.$data.loading = false
+                    }, 3000 )
+                } else {
+                    // console.log( '禁止再加载' )
+                    this.$data.loading = false
+                }
             }
         },
-        // 目的: 下拉自动加载
-        loadMore() {
-            this.loading = true                             // 改变状态
-            let num = this.$data.loadNum                    // 加载次数
-
-            // let residueNum = this.$data.lengthNum 
-
-
-            setTimeout( () => {
-                for( let i = 0; i < 10; i++ ) {
-                    let newBulletinObj = this.$props.bulletinLineArr[ 10 * num + i ]
-                    this.$data.renderArr.push( newBulletinObj )
-                }
-            }, 3000 )
-            this.$data.loadNum = this.$data.loadNum + 1     // 加载一次事件, '加载次数' + 1
+        // 目的: 计算 $data 渲染数组
+        countRender( num ) {
+            for( let i = 0; i < num; i++ ) {
+                let addBulletinObj = this.$props.bulletinLineArr[i + 10 * this.$data.loadNum]
+                this.$data.renderArr.push( addBulletinObj )
+            }
+            this.$data.loadNum = this.$data.loadNum + 1                                 // 记录次数
         }
     },
     data() {
         return {
             loading: false,
-            renderArr: [],                  // 用于渲染的数组( 需要将 $props的数据赋值 )
-            lengthNum: 0,                   // 记录 $props中 '公告'数量 
-            loadNum: 1                      // 加载次数
+            canRender: false,                                                           // 可否渲染数据
+            renderArr: [],                                                              // 用于渲染的数组( 需要将 $props的数据赋值 )
+            bulletinNum: 0,                                                             // 记录 $props中 '公告'数量
+            loadNum: 0,                                                                 // 加载次数
+            maxMultiple: 0                                                              // 最大倍数
         }
     },
     watch: {
         bulletinLineArr: function() {
-            this.countBulletinLength()      // 计算 '公告' 数量
-            this.$data.lengthNum = this.$props.bulletinLineArr.length       // 将 '公告' 数量 保存到 $data 中
+            let bulletinLength = this.$props.bulletinLineArr.length
+            if( typeof bulletinLength === 'number' && bulletinLength > 0 ) {
+                this.$data.canRender = true                                             // 可渲染
+                this.$data.bulletinNum = bulletinLength                                 // 保存'公告'数量
+                this.$data.maxMultiple = parseInt( bulletinLength / 10 )                // 保存最大倍数
+                this.countBulletin()
+            } else {
+                this.$data.canRender = false                                            // 不可 -> 显示'无消息'图片
+            }
         }
     }
 }
 </script>
+
+<style lang="sass">
+@import "../../sass/main"
+
+#BulletinLine
+    .BulletinLine--item
+        +REM-padding-TB( $D-autoPadding )
+        +mL( .5rem )
+        +pL( .5rem )
+        border-left: 1px solid $C-title
+        &:first-child
+            padding-top: 0
+        >span:first-of-type
+            +REM( margin-right, $D-autoMargin )
+</style>
