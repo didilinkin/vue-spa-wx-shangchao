652cde79e5fe5d892079a65b72dbdab9c8640ca0
yanxiaodi <929213769@qq.com>
2017-04-27 20:15:36 +0800

    :construction: make 'Binding.vue'

--- src/view/Binding.vue
+++ src/view/Binding.vue
@@ -1,5 +1,23 @@
 // 绑定房间
 <template lang="pug">
-#Building
+#Building.auto--modulePadding
     h1 绑定房间
+    mt-button(
+        disabled="disabled"
+        size="large"
+        type="default"
+    ) 绑定
 </template>
+
+<script>
+    
+</script>
+
+<style lang="sass">
+@import "../sass/main"
+
+#Building
+</style>
+
+// mt-button( type="primary" ) primary
+// mt-button( type="danger" ) danger
