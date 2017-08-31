0b00d1660bb4c4a675768b91555c70b1be6920cf
jijiayue@forevercj.com <jijiayue@forevercj.com>
2017-08-31 17:34:46 +0800

    url 修改

--- src/services/mockAPI.js
+++ src/services/mockAPI.js
@@ -4,25 +4,25 @@
 
 // const BaseUrl = '/mock'                                                 // for local mock test
 
-const url = 'http://127.0.0.1:18081'                                    // 后台链接
+// const url = 'http://127.0.0.1:18081'                                    // 后台链接
 
-const devUrl = 'http://192.168.5.21:18081'                             // 开发后台 url
+const devUrl = 'http://192.168.5.250:18081'                             // 开发后台 url
 
 // const testUrl = 'http://192.168.5.250:18081'                             // 开发后台 url
 
-export const RentUrl = url + '/wx/listCollectRent?clientNum=1'          // 租赁费用链接
+export const RentUrl = devUrl + '/wx/listCollectRent?clientNum=1'          // 租赁费用链接
 
-export const FeeUrl = url + '/wx/listFee?clientNum=1'                   // 费用列表链接
+export const FeeUrl = devUrl + '/wx/listFee?clientNum=1'                   // 费用列表链接
 
-export const pmUrl = url + '/wx/listPropertyFee?clientNum=1'            // 物业费用列表链接
+export const pmUrl = devUrl + '/wx/listPropertyFee?clientNum=1'            // 物业费用列表链接
 
-export const eleUrl = url + '/wx/listWattHour?clientNum=1'           // 电费用列表链接
+export const eleUrl = devUrl + '/wx/listWattHour?clientNum=1'           // 电费用列表链接
 
-export const waterUrl = url + '/wx/listWaterRecord?clientNum=1'         // 水费用列表链接
+export const waterUrl = devUrl + '/wx/listWaterRecord?clientNum=1'         // 水费用列表链接
 
-export const BUILDING_LIST = url + '/wx/buildingList'                       // 绑定
+export const BUILDING_LIST = devUrl + '/wx/buildingList'                       // 绑定
 
-export const BUILDING_REQUIRE =  url + '/wx/bindingRoom'                // 绑定 - 请求绑定
+export const BUILDING_REQUIRE =  devUrl + '/wx/bindingRoom'                // 绑定 - 请求绑定
 
 export const BULLETIN = devUrl + '/wx/listNotice'                       // 公告
 
