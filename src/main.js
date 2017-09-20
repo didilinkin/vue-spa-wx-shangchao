fb15a521360290d76cc9d8c524046c2706961881
shangchaoadmin <admin@example.com>
2017-09-20 12:57:32 +0800

    去掉登陸頁

--- src/main.js
+++ src/main.js
@@ -59,5 +59,5 @@ function isWeiXin() {
 if( isWeiXin() ) {
     console.log( '是来自微信内置浏览器' )
 }else{
-    location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx98c3af70778cd6ce&redirect_uri=https%3A%2F%2Fwx.myysq.com.cn%2Fauth%2Fopenid%3Ftenant_code%3Dpoly%26public_id%3D39d4e5fb-64ac-4d5c-3215-5b9941a7ac49%26return_url%3Dhttp%253A%252F%252Fvmember.myysq.com.cn%252Fpoly%252Fmember%252Fmembership%252Fmember-info%253Fpublic_id%253D39d4e5fb-64ac-4d5c-3215-5b9941a7ac49%26_state%3DFF5XJXwvSffQfL5i&response_type=code&scope=snsapi_base&state=Irc6pPM6BkTvIH0H&component_appid=wx05b2480ca122fbd0&connect_redirect=1#wechat_redirect'
+    console.log( '不是来自微信内置浏览器' )
 }
